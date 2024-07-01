307035a6cf602ba992c22acbb97430a2a244a74a
HarithaMihimal <harithamihimal4@gmail.com>
2024-07-01 23:01:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import News from './Components/News.jsx'
+import './index.css'
+const App = () => {
+  return (
+    <div className='container'>
+      <News/>
+    </div>
+  )
+}
+
+export default App
